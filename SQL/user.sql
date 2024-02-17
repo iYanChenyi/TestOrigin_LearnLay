597f3159663c88b0df88ff2f473d14ce0ae203d2
VoodooChild937 <invalidfreddie@gmail.com>
2024-02-17 22:01:48 +0800

    fix push

--- SQL/user.sql
+++ SQL/user.sql
@@ -3,16 +3,16 @@ create table user
 (
     id            bigint auto_increment comment 'id'
         primary key,
-    username      varchar(256)                       null comment '用户名',
-    avatar_url    varchar(1024)                      null comment '头像',
-    gender        tinyint                            null comment '性别',
-    password varchar(512)                       null comment '密码',
-    phone         varchar(128)                       null comment '电话',
-    email         varchar(512)                       null comment '邮箱',
-    user_status   int      default 0                 null comment '状态',
-    is_delete     tinyint  default 0                 null comment '是否删除',
-    user_role     int      default 0                 not null comment '用户角色，0-普通用户，1-管理员',
-    id_code       varchar(512)                       null comment '编号'
+    username      varchar(256)                       null comment 'username',
+    avatar_url    varchar(1024)                      null comment 'avatar',
+    gender        tinyint                            null comment 'gender',
+    password      varchar(512)                       null comment 'password',
+    phone         varchar(128)                       null comment 'phone',
+    email         varchar(512)                       null comment 'email',
+    user_status   int      default 0                 null comment 'user_status',
+    is_delete     tinyint  default 0                 null comment 'is_delete',
+    user_role     int      default 0                 not null comment 'user_role，0-user，1-admin',
+    id_code       varchar(512)                       null comment 'id_code'
 )
-    comment '用户';
+    comment 'user';
 
